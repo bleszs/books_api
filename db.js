46df961266d4758fa2e92cfca8881327a8706b9f
Bleszs <bleszs@gmail.com>
2025-11-24 15:56:37 +0700

    ini commit gua cui

---
+++ db.js
@@ -0,0 +1,16 @@
+const { Pool } = require('pg');
+const dotenv = require('dotenv'); // variabel global
+
+dotenv.config();
+const pool = new Pool({
+    user: process.env.PGUSER,
+    host: process.env.PGHOST,
+    database: process.env.PGDATABASE,
+    password: process.env.PGPASSWORD,
+    ssl: {
+        rejectUnauthorized: false,
+    },
+    // port: process.env.PGPORT,
+});
+
+module.exports = pool;
